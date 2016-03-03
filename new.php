beeee5f72af02dd391053328e0975a9a2a2c3820
Gretter74 <bo.sosti@gmail.com>
2016-03-03 10:36:06 +0100

    First work on new.php
    Adding basic form and call to functions.php

---
+++ new.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="da">
+<header>
+<meta charset="UTF-8">
+<title>Ny øl</title>
+</header>
+<body>
+<?php
+include_once "functions.php";
+
+if(isset($_POST['add']))
+   {
+   	// 1: do some error checking here
+   	// 2: throw an error or do DB INSERT here
+	
+   	// 3: set variable that displays "edit" button:
+   	$display_button_valu = "edit";
+   	$display_button_name = "edit";
+   	// 4: set a confirmation message:
+   	$msg = "Your stuff has been ADDED";
+   }
+   // user is editing stuff:
+   elseif(isset($_POST['edit']))
+   {
+   	// 1: do some error checking here
+   	// 2: throw an error or do DB UPDATE here
+   	// 3: set variable that displays "delete" button:
+   	$display_button_valu = "delete";
+   	$display_button_name = "delete";
+   	// 4: set a confirmation message:
+   	$msg = "Your stuff has been EDITED";
+   }
+   // user is deleting stuff:
+   elseif(isset($_POST['delete']))
+   {
+   	// 1: do some error checking here
+   	// 2: throw an error or do DB DELETE here
+   	// 3: set variable that displays "add" button:
+   	$display_button_valu = "add";
+   	$display_button_name = "add";
+   	// 4: set a confirmation message:
+   	$msg = "Your stuff has been DELETED";
+   }
+   // user is doing nothing, it's his/her first time (ooer)
+   else
+   {
+   	// 1: set variable that displays "add" button:
+   	$display_button_valu = "add";
+   	$display_button_name = "add";
+   	// 2: set a welcome message:
+   	$msg = "Welcome. Please add some stuff in the form below";
+   }
+?>
+
+<form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+Brygger: <input type="text" name="brewery"><br>
+Øl navn: <input type="text" name="beer"><br>
+Type: <input type="text" name="type"><br>
+Stregkode <input type="text" name="barcode"><br>
+<input type="submit" name="<?=$display_button_name?>" value="<?=$display_button_valu?>" />
+
+</form>
+
+
+</body>
+</html>
