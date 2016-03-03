65b73f8d2cdceb93c2ad0263df88fbb102a75e36
gretter74 <bo.sosti@gmail.com>
2016-03-03 09:26:45 +0000

    Work on functions.php and new.php has started.

--- install/install.php
+++ install/install.php
@@ -2,5 +2,19 @@
 
 - create tables
 - insert data
+- update config.php to match your installation.
 
 -->
+
+<?php
+include "../functions.php";
+
+#Read configuration.
+
+#Connect to db.db.
+connect_db($status, $conn);
+if ($status != 1)
+    die($status);
+
+#Execute create_tables.sql.
+?>
